070f20bc52efef0de097e6512e9d27cb3b0f0420
Pranay <pranayvjadhao197@gmail.com>
2026-05-27 08:08:28 +0530

    Initial commit

---
+++ excp.py
@@ -0,0 +1,14 @@
+print("Program starts...")
+
+try:
+    a = int(input("Enter first number : "))
+    b = int(input("Enter second number : "))
+
+    c = a / b
+
+    print("The division of", a, "and", b, "is", c)
+
+except Exception:
+    print("Error in data")
+
+print("Program ends...")
